1b21afcc925a5949dd5063d98fb30cd711329fa0
shubham sharma <sharmashubham33@gmail.com>
2023-11-27 00:47:09 -0500

    Initial Commit

--- script.js
+++ script.js
@@ -4,20 +4,38 @@ const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
-yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 18th!";
-  gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+// Play sound on hover over "No" button
+noBtn.addEventListener("mouseover", () => {
+    const sound = new Audio("meow.mp3");
+    sound.play();
+
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
 });
 
-noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
+// Play sound and show confetti when user clicks "Yes"
+yesBtn.addEventListener("click", () => {
+    const sound = new Audio("omgwow.mp3");
+    sound.play();
 
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
+    question.innerHTML = "Yay, see you on the 5th March!";
+    gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
+    for (let i = 0; i < 30; i++) {
+        const confetti = document.createElement("div");
+        confetti.className = "confetti";
+        confetti.style.left = Math.random() * window.innerWidth + "px";
+        confetti.style.animationDuration = Math.random() * 2 + 1 + "s";
+        document.body.appendChild(confetti);
+        confetti.addEventListener("animationend", () => {
+            confetti.remove();
+        });
+    }
 });
